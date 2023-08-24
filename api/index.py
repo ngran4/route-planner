f206b0dd6e8f6e72c95af5d6c2929024fa310b63
Jordan Bank <jordan.w.bank@gmail.com>
2023-08-24 14:52:00 -0400

    stuff n shit

--- api/index.py
+++ api/index.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models import User
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+db = SQLAlchemy())
 
-@app.route("/api/healthchecker", methods=["GET"])
-def healthchecker():
-    return {"status": "success", "message": "Integrate Flask Framework with Next.js"}
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:FXZ2v3TmracgceRKluVH@containers-us-west-35.railway.app:7698/railway"
+    db.init_app(app)
+
+    return app
+
+@app.route("/api/users", methods=['GET'])
+def get_user():
+    users = User.query.all()
+    user_list = []
+
+    for user in users:
+        data = {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email
+        }
+        user_list.append(data)
+    
+    return jsonify(user_list)
+
+@app.route('/api/users', methods=['POST'])
+def create_user():
+    data = request.json
+
+    new_user=User(id=35, username=data['username'], email=data['email'])
+
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"done"})
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
