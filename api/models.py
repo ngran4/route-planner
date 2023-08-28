5a25b86b936fca5b4f2fc575da0f4e4591a8ac89
Jordan Bank <jordan.w.bank@gmail.com>
2023-08-28 14:15:40 -0400

    oh shit i accidentally removed a line

--- api/models.py
+++ api/models.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
+
 DATABASE_URI = os.environ.get('DATABASE_URI')
-print(DATABASE_URI)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
