e06d7013436630ac37896dba63434e96c02f3162
ngran4 <nicgranville@gmail.com>
2023-08-29 17:22:16 -0700

    fix local storage error

--- src/app/page.js
+++ src/app/page.js
@@ -1,48 +1,48 @@
-"use client"
-import React, {useState} from 'react';
-import Login from './login'
-import Register from './register'
+import React, { useState, useEffect } from 'react';
+import Login from './login';
+import Register from './register';
 
 export default function Home() {
   const [activeForm, setActiveForm] = useState('login');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const toggleForm = () => {
     setActiveForm(prevForm => (prevForm === 'login' ? 'signup' : 'login'));
   };
 
-
-  const [activeComponent, setActiveComponent] = useState('login');
-
-  const handleButtonClick = (component) => {
+  const handleButtonClick = component => {
     setActiveComponent(component);
   };
 
-
   const logout = () => {
-    if (typeof window !== "undefined") {
-    localStorage.removeItem('token')
-    sessionStorage.removeItem('token')
-    console.log("Token removed!")
-    window.location.reload()
-    }
-  }
+    localStorage.removeItem('token');
+    sessionStorage.removeItem('token');
+    console.log("Token removed!");
+    window.location.reload();
+  };
 
-  if (localStorage.getItem('token')){
-    return  (
-    <>
-    <p>Hi {localStorage.getItem('name')}! You are logged in.</p>
-    <button onClick={logout}>Logout</button>
-    </>
-    )
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  if (isLoggedIn) {
+    return (
+      <>
+        <p>Hi {localStorage.getItem('name')}! You are logged in.</p>
+        <button onClick={logout}>Logout</button>
+      </>
+    );
   } else {
     return (
       <>
-          {activeForm === 'login' ? (
-      <Login showRegister={toggleForm}/>
-    ) : (
-      <Register showLogin={toggleForm} />
-    )}
+        {activeForm === 'login' ? (
+          <Login showRegister={toggleForm} />
+        ) : (
+          <Register showLogin={toggleForm} />
+        )}
       </>
-    )
+    );
   }
 }
