ec8f2521a52627a9d0c1120aa6aca787e1205e1d
Jordan Bank <jordan.w.bank@gmail.com>
2023-09-20 14:22:06 -0400

    map stuff

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,10 +1,29 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
 
 export default function Home() {
-  return (
-    <>
-    foop
-    </>
-  )
+  const mapRef = useRef();
+
+  useEffect(() => {
+    const loader = new Loader({
+      apiKey: "AIzaSyAyim66JOKCkBCBbebk0P_JEaj395fUIaE",
+      version: "weekly",
+    });
+
+    loader
+      .load()
+      .then((google) => {
+        new google.maps.Map(mapRef.current, {
+          center: { lat: -34.397, lng: 150.644 },
+          zoom: 8,
+        });
+      })
+      .catch((err) => {
+        // Handle any errors
+        console.error(err);
+      });
+  }, []);
+
+  return <div id="map" ref={mapRef} style={{ width: "100%", height: "100vh" }} />;
 }
